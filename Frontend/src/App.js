6a60cf31986286707fc304aad82d53b9d304199f
pnhotro <truongchibao1503@gmail.com>
2024-05-16 21:15:53 +0700

    xong Login

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,16 +1,24 @@
+
+
 // import { Navigate } from "react-router-dom";
+// import { useContext } from "react";
 import "./App.css";
-// import { AuthProvider, useAuth } from "./AuthContext";
+// import { AuthContext, AuthProvider, useAuth } from "./AuthContext";
 import AllRoute from "./components/AllRoute";
-import { Navigate  } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 function App() {
 
+  // const { isLoggedIn } = useContext(AuthContext);
+
+  // if (!isLoggedIn) {
+  //   // return <Navigate to="login" />;
+  //   window.location.href = '/login';
+  // }
+
   return (
     <>
-      {/* <AuthProvider> */}
-        <AllRoute />
-      {/* </AuthProvider> */}
+      <AllRoute />
     </>
   );
 }
@@ -18,7 +26,6 @@ function App() {
 export function PrivateRoute({ component }) {
   const isAuthenticated = sessionStorage.getItem('isAuthenticated');
   console.log(isAuthenticated);
-  // const { isLoggedIn } = useAuth();
 
   if (!isAuthenticated) {
     return <Navigate to="/login" />;
@@ -26,6 +33,5 @@ export function PrivateRoute({ component }) {
 
   return component;
 }
-
 
 export default App;
--- Frontend/src/pages/Login/index.js
+++ Frontend/src/pages/Login/index.js
@@ -1,52 +1,78 @@
-// components/Login.js
-
-import { useState } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-// import { useAuth } from "../../AuthContext";
+
+import axios from "axios";
 
 function Login() {
-  
   const navigate = useNavigate();
-  // const { login } = useAuth();
-  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  // const [error, setError] = useState("");
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    if (username === 'admin' && password === '123') {
+    try {
+      const response = await axios.post("http://localhost:4060/api/auth/signin", { email, password });
+      localStorage.setItem("tokens", response.data.token);
       sessionStorage.setItem('isAuthenticated', 'true');
-      // login();
-      
+      //login();
       navigate("/");
-      alert('Đăng nhập thành công!');
-    } else {
-      alert('Thông tin đăng nhập không đúng');
+    } catch (error) {
+      if (error.response) {
+        alert(error.response.data.message);
+      }
+      console.error("Đăng nhập thất bại:", error);
     }
   };
 
   return (
-    <div className="container">
-      <form className="form" onSubmit={handleSubmit}>
-        <h2>Login</h2>
-        <div>
-          <label>Username:</label>
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
+    <div className="login-page">
+      <div className="login ">
+        <form onSubmit={handleSubmit}>
+          <div className="box">
+            <div className="container">
+              <div className="top-header">
+              </div>
+              <div className="input-field">
+                <input
+                  className="input"
+                  placeholder="Email"
+                  type="text"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+                
+                <i className="bx bx-user"></i>
+              </div>
+              <div className="input-field">
+                
+                <input
+                  type="password"
+                  className="input"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+               
+                <i className="bx bx-lock-alt"></i>
+              </div>
+              <div className="input-field">
+                <input type="submit" className="submit-login" value="Login" />
+              </div>
+              <div className="bottom">
+                <div className="right">
+                  <label><a href="/register">Already have an account?</a></label>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </form>
+      </div>
     </div>
+
   );
 }
 
